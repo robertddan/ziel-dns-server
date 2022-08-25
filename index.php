529ca3da3a7875e6aa9dc5505480548f950bd5dd
Robert-Dan <danrobert445@gmail.com>
2022-08-25 18:29:43 +0000

    message

--- index.php
+++ index.php
@@ -13,7 +13,24 @@ while(true) {
 	$aData = array();
 
 	var_dump([$buf, count($aBuffer)]);
-	
+# Get the flags
+/*
+                                    1  1  1  1  1  1
+      0  1  2  3  4  5  6  7  8  9  0  1  2  3  4  5
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |                      ID                       |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |QR|   Opcode  |AA|TC|RD|RA|   Z    |   RCODE   |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |                    QDCOUNT                    |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |                    ANCOUNT                    |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |                    NSCOUNT                    |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |                    ARCOUNT                    |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+*/
 	foreach($aBuffer as $k => $sField)
 	{
 		$sField = base_convert(ord($sField), 10, 2);
@@ -26,57 +43,73 @@ var_dump($k);
 				if (!isset($sTxId)) $sTxId = "";
 				$sTxId .= base_convert($sField, 2, 16);
 				var_dump(['tx id:', $sTxId]);
+				array_pop($aBuffer);
 			break;
 			case 2:
 				#1 QR, 4 Opcode, 1 AA, 1 TC, 1 RD
 				$aFields = str_split($sField);
 				$aQuery = array(
-					'QR' => base_convert($aFields[0], 2, 16),
-					'Opcode' => base_convert(implode(array_slice($aFields, 1, 4)), 2, 16),
-					'AA' => base_convert($aFields[5], 2, 16),
-					'TC' => base_convert($aFields[6], 2, 16),
-					'RD' => base_convert($aFields[7], 2, 16)
+					'QR' => base_convert($aFields[0], 2, 10),
+					'Opcode' => base_convert(implode(array_slice($aFields, 1, 4)), 2, 10),
+					'AA' => base_convert($aFields[5], 2, 10),
+					'TC' => base_convert($aFields[6], 2, 10),
+					'RD' => base_convert($aFields[7], 2, 10)
 				);
 				var_dump(['2 query:', $aQuery]);
+				array_pop($aBuffer);
 			break;
 			case 3:
 				# 1 RA, 3 Z, 4 RCODE
 				$aFields = str_split($sField);
-				var_dump(array(
-					'RA' => base_convert($aFields[1], 2, 16),
-					'Z' => array_slice($aFields, 1, 3),
-					'RCODE' => array_slice($aFields, 4, 4),
-				));
 				$aQuery = array(
-					'RA' => base_convert($aFields[1], 2, 16),
-					'Z' => base_convert(implode(array_slice($aFields, 1, 3)), 2, 16),
-					'RCODE' => base_convert(implode(array_slice($aFields, 4, 4)), 2, 16),
+					'RA' => base_convert($aFields[1], 2, 10),
+					'Z' => base_convert(implode(array_slice($aFields, 1, 3)), 2, 10),
+					'RCODE' => base_convert(implode(array_slice($aFields, 4, 4)), 2, 10),
 				);
 				var_dump(['3 query:', $aQuery]);
+				array_pop($aBuffer);
+			break;
+			case 4: # QDCOUNT
+			case 5:
+				if (!isset($sTxId45)) $sTxId45 = "";
+				$sTxId45 .= base_convert($sField, 2, 16);
+				var_dump(['4 5 QDCOUNT:', $sTxId45]);
+				array_pop($aBuffer);
+			break;
+			case 6: # ANCOUNT
+			case 7:
+				if (!isset($sTxId67)) $sTxId67 = "";
+				$sTxId67 .= base_convert($sField, 2, 16);
+				var_dump(['6 7 ANCOUNT:', $sTxId67]);
+				array_pop($aBuffer);
+			break;
+			case 8: # NSCOUNT
+			case 9:
+				if (!isset($sTxId89)) $sTxId89 = "";
+				$sTxId89 .= base_convert($sField, 2, 16);
+				var_dump(['8 9 NSCOUNT:', $sTxId89]);
+				array_pop($aBuffer);
+			break;
+			case 10: # ARCOUNT
+			case 11:
+				if (!isset($sTxIdab)) $sTxIdab = "";
+				$sTxIdab .= base_convert($sField, 2, 16);
+				var_dump(['a b ARCOUNT:', $sTxIdab]);
+				array_pop($aBuffer);
+			break;
+			case 12: # question
+			case 13:
+				if (!isset($sTxIdcd)) $sTxIdcd = "";
+				$sTxIdcd .= base_convert($sField, 2, 16);
+				var_dump(['c d QNAME:', $sTxIdcd]);
+				array_pop($aBuffer);
 			break;
 		}
 	}
-	
+	var_dump(array('$aBuffer', $aBuffer));
 	var_dump(implode(" ", $aData));
 
-# Get the flags
-/*
-	var_dump($a) .PHP_EOL;
-	$stz = bin2hex($buf);
-	$tx = "";
-	
-	for($i=0;$i<(strlen($stz)-26-10)/2;$i++)
-	{
-		$e = "00";
-		$e[0] = $stz[$i*2+26];
-		$e[1] = $stz[$i*2+27];
-		$f = hexdec($e);
-		if($f > 0 && $f < 32) $tx .= "."; else
-		$tx .= sprintf("%c",$f);
-	}
-	
-	echo "$clientIP <".$tx.">\n";
-*/
+
 	
 }
 socket_send($socket,$ret,667,0);
