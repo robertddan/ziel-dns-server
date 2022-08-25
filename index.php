2b9038abed7b877d7ae1ad911e648cb1a458cade
Robert-Dan <danrobert445@gmail.com>
2022-08-25 13:52:49 +0000

    message

--- index.php
+++ index.php
@@ -8,7 +8,18 @@ while(true) {
 		print_r(socket_strerror(socket_last_error())).PHP_EOL;
 	}
 	socket_recvfrom($socket, $buf, 65535, 0, $clientIP, $clientPort);
-	var_dump($buf);
+   $stz = bin2hex($buf);
+   $tx = "";
+   for($i=0;$i<(strlen($stz)-26-10)/2;$i++)
+   {
+     $e = "00";
+     $e[0] = $stz[$i*2+26];
+     $e[1] = $stz[$i*2+27];
+     $f = hexdec($e);
+     if($f > 0 && $f < 32) $tx .= "."; else
+     $tx .= sprintf("%c",$f);
+   }
+   echo "$clientIP <".$tx.">\n";   
 }
 socket_send($socket,$ret,667,0);
 
